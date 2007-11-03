1e4ff4dc501ca82490ac70fb3b623c3857991af3
Lars Hjemli <hjemli@gmail.com>
2007-11-03 11:33:28 +0100

    Don't include current SHA1 in 'log' menu-item
    This ensures that the menu-item always prints the log for the active branch.
    Predictability is good.
    
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>

--- ui-shared.c
+++ ui-shared.c
@@ -473,8 +473,8 @@ void cgit_print_pageheader(char *title, int show_search)
 		html("<p>\n<h1>navigate</h1>\n");
 		reporevlink(NULL, "summary", NULL, "menu", cgit_query_head,
 			    NULL, NULL);
-		cgit_log_link("log", NULL, "menu", cgit_query_head,
-			      cgit_query_sha1, NULL, 0, NULL, NULL);
+		cgit_log_link("log", NULL, "menu", cgit_query_head, NULL, NULL,
+			      0, NULL, NULL);
 		cgit_tree_link("tree", NULL, "menu", cgit_query_head,
 			       cgit_query_sha1, NULL);
 		cgit_commit_link("commit", NULL, "menu", cgit_query_head,
