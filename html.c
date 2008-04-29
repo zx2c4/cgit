112b2080626c62fff27cf8aaa9ac2fb07eb50b74
Harley Laue <losinggeneration@aim.com>
2008-04-29 17:59:53 +0200

    Print an error if filename is not found in html_include.
    Normally when html_include cannot open the file it fails silently and
    things can be a bit hard to figure out from just looking at apache's
    log. This will be beneficial for those initially setting up their server
    with cgit.
    
    Signed-off-by: Harley Laue <losinggeneration@aim.com>
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>

--- html.c
+++ html.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <errno.h>
 
 int htmlfd = STDOUT_FILENO;
 
@@ -168,8 +169,11 @@ int html_include(const char *filename)
 	char buf[4096];
 	size_t len;
 
-	if (!(f = fopen(filename, "r")))
+	if (!(f = fopen(filename, "r"))) {
+		fprintf(stderr, "[cgit] Failed to include file %s: %s (%d).\n",
+			filename, strerror(errno), errno);
 		return -1;
+	}
 	while((len = fread(buf, 1, 4096, f)) > 0)
 		write(htmlfd, buf, len);
 	fclose(f);
