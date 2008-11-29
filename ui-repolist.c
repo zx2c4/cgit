54272e60965ec6a98b49cbf67d72a4b1f5adc55b
Lars Hjemli <hjemli@gmail.com>
2008-11-29 14:27:35 +0100

    ui-repolist: sort null values last
    When sorting on e.g. owner, it's not interesting to get all repos
    without owner at the top of the list.
    
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>

--- ui-repolist.c
+++ ui-repolist.c
@@ -116,9 +116,9 @@ static int cmp(const char *s1, const char *s2)
 	if (s1 && s2)
 		return strcmp(s1, s2);
 	if (s1 && !s2)
-		return 1;
-	if (s2 && !s1)
 		return -1;
+	if (s2 && !s1)
+		return 1;
 	return 0;
 }
 
