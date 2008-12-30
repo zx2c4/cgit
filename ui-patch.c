03afc5fe1fe70b6e44b60708c89708060e6d5a90
Tomas Carnecky <tom@dbservice.com>
2008-12-30 11:14:52 +0100

    ui-patch: whitespace changes in the patch generation code
    Add a space between the committer name and email, and remove
    superfluous spaces in the date header.
    
    This makes cgit-generated patches match the output from git-format-patch
    almost exactly, at least as far as the email headers go.
    
    Signed-off-by: Tomas Carnecky <tom@dbservice.com>
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>

--- ui-patch.c
+++ ui-patch.c
@@ -101,9 +101,9 @@ void cgit_print_patch(char *hex)
 	ctx.page.filename = patchname;
 	cgit_print_http_headers(&ctx);
 	htmlf("From %s Mon Sep 17 00:00:00 2001\n", sha1_to_hex(sha1));
-	htmlf("From: %s%s\n", info->author, info->author_email);
+	htmlf("From: %s %s\n", info->author, info->author_email);
 	html("Date: ");
-	cgit_print_date(info->author_date, "%a, %d  %b  %Y  %H:%M:%S  %z%n", ctx.cfg.local_time);
+	cgit_print_date(info->author_date, "%a, %d %b %Y %H:%M:%S %z%n", ctx.cfg.local_time);
 	htmlf("Subject: %s\n\n", info->subject);
 	if (info->msg && *info->msg) {
 		htmlf("%s", info->msg);
