f9143171267e39f66a02ad2ecb0da2e8cc7f4142
Jeff Smith <whydoubt@yahoo.com>
2010-07-05 22:38:34 -0500

    commit-links.sh: Seperate the expressions for filtering commit messages.
    This allows for putting descriptions closer to their expressions.  It
    should also make it clearer how to apply an expression conditionally.

--- filters/commit-links.sh
+++ filters/commit-links.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-# This script can be used to generate links in commit messages - the first
-# sed expression generates links to commits referenced by their SHA1, while
-# the second expression generates links to a fictional bugtracker.
+# This script can be used to generate links in commit messages.
 #
 # To use this script, refer to this file with either the commit-filter or the
 # repo.commit-filter options in cgitrc.
 
-sed -re '
-s|\b([0-9a-fA-F]{8,40})\b|<a href="./?id=\1">\1</a>|g
-s| #([0-9]+)\b|<a href="http://bugs.example.com/?bug=\1">#\1</a>|g
-'
+# This expression generates links to commits referenced by their SHA1.
+regex=$regex'
+s|\b([0-9a-fA-F]{8,40})\b|<a href="./?id=\1">\1</a>|g'
+# This expression generates links to a fictional bugtracker.
+regex=$regex'
+s| #([0-9]+)\b|<a href="http://bugs.example.com/?bug=\1">#\1</a>|g'
+
+sed -re "$regex"
