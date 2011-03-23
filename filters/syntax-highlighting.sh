b2cf630a4b423bbda6507b7f658042563e76b36e
Ferry Huberts <ferry.huberts@pelagic.nl>
2011-03-23 11:57:44 +0100

    filters: document environment variables in filter scripts
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>
    Signed-off-by: Lars Hjemli <hjemli@gmail.com>

--- filters/syntax-highlighting.sh
+++ filters/syntax-highlighting.sh
@@ -23,6 +23,17 @@
 # table.blob .kwb  { color:#830000; }
 # table.blob .kwc  { color:#000000; font-weight:bold; }
 # table.blob .kwd  { color:#010181; }
+#
+# The following environment variables can be used to retrieve the configuration
+# of the repository for which this script is called:
+# CGIT_REPO_URL        ( = repo.url       setting )
+# CGIT_REPO_NAME       ( = repo.name      setting )
+# CGIT_REPO_PATH       ( = repo.path      setting )
+# CGIT_REPO_OWNER      ( = repo.owner     setting )
+# CGIT_REPO_DEFBRANCH  ( = repo.defbranch setting )
+# CGIT_REPO_SECTION    ( = section        setting )
+# CGIT_REPO_CLONE_URL  ( = repo.clone-url setting )
+#
 
 # store filename and extension in local vars
 BASENAME="$1"
