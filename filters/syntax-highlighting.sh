375353caffb0a251e7f0174aa9b73cc1961e93fd
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-03-18 11:38:26 +0000

    filters/highlight.sh: manually support highlight version 2 and 3

--- filters/syntax-highlighting.sh
+++ filters/syntax-highlighting.sh
@@ -31,4 +31,15 @@ EXTENSION="${BASENAME##*.}"
 # map Makefile and Makefile.* to .mk
 [ "${BASENAME%%.*}" == "Makefile" ] && EXTENSION=mk
 
+# highlight versions 2 and 3 have different commandline options. Specifically,
+# the -X option that is used for version 2 is replaced by the -O xhtml option
+# for version 3.
+#
+# Version 2 can be found (for example) on EPEL 5, while version 3 can be
+# found (for example) on EPEL 6.
+#
+# This is for version 2
 exec highlight --force -f -I -X -S $EXTENSION 2>/dev/null
+
+# This is for version 3
+#exec highlight --force -f -I -O xhtml -S $EXTENSION 2>/dev/null
