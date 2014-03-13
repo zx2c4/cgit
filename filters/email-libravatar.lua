e22e985416929adb735bd952db00fce82d883914
Christian Hesse <mail@eworm.de>
2014-03-13 11:55:49 +0100

    filter: add libravatar email-filter lua script

---
+++ filters/email-libravatar.lua
@@ -0,0 +1,26 @@
+-- This script may be used with the email-filter or repo.email-filter settings in cgitrc.
+-- It adds libravatar icons to author names. It is designed to be used with the lua:
+-- prefix in filters.
+--
+-- Requirements:
+-- 	luacrypto >= 0.3
+-- 	<http://mkottman.github.io/luacrypto/>
+--
+
+local crypto = require("crypto")
+
+function filter_open(email, page)
+	buffer = ""
+	md5 = crypto.digest("md5", email:sub(2, -2):lower())
+end
+
+function filter_close()
+	html("<img src='//cdn.libravatar.org/avatar/" .. md5 .. "?s=13&amp;d=retro' width='13' height='13' alt='Libravatar' /> " .. buffer)
+	return 0
+end
+
+function filter_write(str)
+	buffer = buffer .. str
+end
+
+
