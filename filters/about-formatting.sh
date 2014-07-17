62affc0e91f3f322f0b7c27f67f527845e857c82
Chris Burroughs <chris.burroughs@gmail.com>
2014-07-17 09:44:13 -0400

    match other common markdown file extensions

--- filters/about-formatting.sh
+++ filters/about-formatting.sh
@@ -19,7 +19,7 @@
 
 cd "$(dirname $0)/html-converters/"
 case "$(printf '%s' "$1" | tr '[:upper:]' '[:lower:]')" in
-	*.md|*.mkd) exec ./md2html; ;;
+	*.markdown|*.mdown|*.md|*.mkd) exec ./md2html; ;;
 	*.rst) exec ./rst2html; ;;
 	*.[1-9]) exec ./man2html; ;;
 	*.htm|*.html) exec cat; ;;
