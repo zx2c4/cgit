80d52079f7f58d273d99745f93909e5c6524a9bd
John Keeping <john@keeping.me.uk>
2015-03-08 16:32:26 +0000

    cache: don't use an integer as a NULL pointer
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- cache.c
+++ cache.c
@@ -411,7 +411,7 @@ int cache_ls(const char *path)
 	DIR *dir;
 	struct dirent *ent;
 	int err = 0;
-	struct cache_slot slot = { 0 };
+	struct cache_slot slot = { NULL };
 	struct strbuf fullname = STRBUF_INIT;
 	size_t prefixlen;
 
