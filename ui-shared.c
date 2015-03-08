94e5f212f55d3f4dd6ce83edb6db823de7eb8cc7
John Keeping <john@keeping.me.uk>
2015-03-08 16:32:24 +0000

    ui-shared: avoid initializing static variable to zero
    Sparse complains that we are using a plain integer as a NULL pointer
    here, but in fact we do not have to specify a value for this variable at
    all since it has static storage duration and thus will be initialized to
    NULL by the compiler.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- ui-shared.c
+++ ui-shared.c
@@ -82,7 +82,7 @@ const char *cgit_rooturl(void)
 
 const char *cgit_loginurl(void)
 {
-	static const char *login_url = 0;
+	static const char *login_url;
 	if (!login_url)
 		login_url = fmtalloc("%s?p=login", cgit_rooturl());
 	return login_url;
