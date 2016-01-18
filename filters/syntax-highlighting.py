d3756bd7b00f9ad6adede3c7f956a25b22a2254a
Jason A. Donenfeld <Jason@zx2c4.com>
2016-01-18 11:14:06 +0100

    syntax-highlighting: always use utf-8 to avoid ascii codec issues

--- filters/syntax-highlighting.py
+++ filters/syntax-highlighting.py
@@ -21,6 +21,7 @@
 
 
 import sys
+import io
 from pygments import highlight
 from pygments.util import ClassNotFound
 from pygments.lexers import TextLexer
@@ -29,6 +30,8 @@ from pygments.lexers import guess_lexer_for_filename
 from pygments.formatters import HtmlFormatter
 
 
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 data = sys.stdin.read()
 filename = sys.argv[1]
 formatter = HtmlFormatter(style='pastie')
