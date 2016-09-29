28185ae40a82d3304ace805c9a44e933270bd581
Christian Hesse <mail@eworm.de>
2016-09-29 22:14:28 +0200

    ui-tag: replace 'unsigned char sha1[20]' with 'struct object_id oid'
    Upstream git is replacing 'unsigned char sha1[20]' with 'struct object_id
    oid'. We have some code that can be changed independent from upstream. So
    here we go...

--- ui-tag.c
+++ ui-tag.c
@@ -42,29 +42,29 @@ static void print_download_links(char *revname)
 void cgit_print_tag(char *revname)
 {
 	struct strbuf fullref = STRBUF_INIT;
-	unsigned char sha1[20];
+	struct object_id oid;
 	struct object *obj;
 
 	if (!revname)
 		revname = ctx.qry.head;
 
 	strbuf_addf(&fullref, "refs/tags/%s", revname);
-	if (get_sha1(fullref.buf, sha1)) {
+	if (get_oid(fullref.buf, &oid)) {
 		cgit_print_error_page(404, "Not found",
 			"Bad tag reference: %s", revname);
 		goto cleanup;
 	}
-	obj = parse_object(sha1);
+	obj = parse_object(oid.hash);
 	if (!obj) {
 		cgit_print_error_page(500, "Internal server error",
-			"Bad object id: %s", sha1_to_hex(sha1));
+			"Bad object id: %s", oid_to_hex(&oid));
 		goto cleanup;
 	}
 	if (obj->type == OBJ_TAG) {
 		struct tag *tag;
 		struct taginfo *info;
 
-		tag = lookup_tag(sha1);
+		tag = lookup_tag(oid.hash);
 		if (!tag || parse_tag(tag) || !(info = cgit_parse_tag(tag))) {
 			cgit_print_error_page(500, "Internal server error",
 				"Bad tag object: %s", revname);
@@ -74,7 +74,7 @@ void cgit_print_tag(char *revname)
 		html("<table class='commit-info'>\n");
 		htmlf("<tr><td>tag name</td><td>");
 		html_txt(revname);
-		htmlf(" (%s)</td></tr>\n", sha1_to_hex(sha1));
+		htmlf(" (%s)</td></tr>\n", oid_to_hex(&oid));
 		if (info->tagger_date > 0) {
 			html("<tr><td>tag date</td><td>");
 			html_txt(show_date(info->tagger_date, info->tagger_tz,
