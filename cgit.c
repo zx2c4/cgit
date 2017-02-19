7d9b5590f4ffbc5d8a6fe30e3e9715ad4b5ca7bd
John Keeping <john@keeping.me.uk>
2017-02-19 12:02:37 +0000

    cgit: don't set vpath unless repo is set
    After the previous two patches, this can be classified as a tidy up
    rather than a bug fix, but I think it makes sense to group all of the
    tests together before setting up the environment for the command to
    execute.
    
    Signed-off-by: John Keeping <john@keeping.me.uk>

--- cgit.c
+++ cgit.c
@@ -726,17 +726,17 @@ static void process_request(void)
 		return;
 	}
 
-	/* If cmd->want_vpath is set, assume ctx.qry.path contains a "virtual"
-	 * in-project path limit to be made available at ctx.qry.vpath.
-	 * Otherwise, no path limit is in effect (ctx.qry.vpath = NULL).
-	 */
-	ctx.qry.vpath = cmd->want_vpath ? ctx.qry.path : NULL;
-
 	if (cmd->want_repo && !ctx.repo) {
 		cgit_print_error_page(400, "Bad request",
 				"No repository selected");
 		return;
 	}
+
+	/* If cmd->want_vpath is set, assume ctx.qry.path contains a "virtual"
+	 * in-project path limit to be made available at ctx.qry.vpath.
+	 * Otherwise, no path limit is in effect (ctx.qry.vpath = NULL).
+	 */
+	ctx.qry.vpath = cmd->want_vpath ? ctx.qry.path : NULL;
 
 	if (ctx.repo && prepare_repo_cmd())
 		return;
