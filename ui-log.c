zx2c4/cgit

commit bb02e24ec23d7f1893fc746c8199e88ab849cf86

git: update to v2.36.0
Update to git version v2.36.0, this requires changes for these
upstream commits:

* 95433eeed9eac439eb21eb30105354b15e71302e
  diff: add ability to insert additional headers for paths

Signed-off-by: Christian Hesse <mail@eworm.de>

ui-log.c
@@ -159,7 +159,7 @@ static int show_commit(struct commit *commit, struct rev_info *revs)
 		      "", &revs->diffopt);
 	diffcore_std(&revs->diffopt);
 
-	found = !diff_queue_is_empty();
+	found = !diff_queue_is_empty(&revs->diffopt);
 	saved_fmt = revs->diffopt.output_format;
 	revs->diffopt.output_format = DIFF_FORMAT_CALLBACK;
 	revs->diffopt.format_callback = cgit_diff_tree_cb;

Makefile
@@ -14,7 +14,7 @@ htmldir = $(docdir)
 pdfdir = $(docdir)
 mandir = $(prefix)/share/man
 SHA1_HEADER = <openssl/sha.h>
-GIT_VER = 2.35.3
+GIT_VER = 2.36.0
 GIT_URL = https://www.kernel.org/pub/software/scm/git/git-$(GIT_VER).tar.xz
 INSTALL = install
 COPYTREE = cp -r

commit 29d8c62437c71410061daa6a1b3ec0ec566eb559

ui-log: don't free diff object when outputting diff
In git 2.36, the logic for diff_flush() changed, resulting in our object
getting freed, which led to following renames producing garbage commits
until it eventually crashed.

Fixes: bb02e24 ("git: update to v2.36.0")
Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

ui-log.c
@@ -167,6 +167,7 @@ static int show_commit(struct commit *commit, struct rev_info *revs)
 	revs->diffopt.output_format = DIFF_FORMAT_CALLBACK;
 	revs->diffopt.format_callback = cgit_diff_tree_cb;
 	revs->diffopt.format_callback_data = handle_rename;
+	revs->diffopt.no_free = 1;
 	diff_flush(&revs->diffopt);
 	revs->diffopt.output_format = saved_fmt;
 	revs->diffopt.flags = saved_flags;